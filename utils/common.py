afe3c78d77fb51f14356b4d6654bab839fe729b5
Kristiyan Sakalyan <kristian.sakalian@gmail.com>
2024-05-02 11:06:01 +0200

    Seed everything

---
+++ utils/common.py
@@ -0,0 +1,17 @@
+import random
+
+import numpy as np
+import torch
+
+
+def set_seed(seed: int) -> None:
+    """Set the random seed for reproducibility.
+
+    Parameters
+    ----------
+    seed : int
+        The random seed to use.
+    """
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
