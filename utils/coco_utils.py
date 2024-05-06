d89d9f490f4e73da1c77ee0cb216fb670651426b
Anar Abbas <anar.abbasly@gmail.com>
2024-05-06 18:39:30 +0200

    add utility to merge coco files which handle possible conflicting id issues

--- utils/coco_utils.py
+++ utils/coco_utils.py
@@ -6,13 +6,13 @@ import shutil
 from pycocotools.coco import COCO
 
 
-class COCOSampler:
+class COCOUtils:
     def __init__(self, original_annotation_file, output_directory):
         self.original_annotation_file = original_annotation_file
         self.output_directory = output_directory
         self.coco = COCO(original_annotation_file)
 
-    def random_sample(self, target_size_mb):
+    def random_sample(self, target_size_mb, directory_path):
         filtered_annotations = []
         filtered_images = []
         total_size_mb = 0
@@ -36,9 +36,7 @@ class COCOSampler:
             filtered_images.append(image_info)
 
             # Construct the original image path
-            original_image_path = os.path.join(
-                "data", "cadis", "training", image_info["file_name"]
-            )
+            original_image_path = os.path.join(directory_path, image_info["file_name"])
 
             image_size_mb = os.path.getsize(original_image_path) / (1024 * 1024)
 
@@ -75,3 +73,57 @@ class COCOSampler:
         # Print the total size of sampled images and the number of sampled images
         print("Total size of sampled images:", total_size_mb, "MB")
         print("Number of sampled images:", num_sampled_images)
+
+    @staticmethod
+    def merge_coco_jsons(main_coco, sampled_coco, output_dir=None):
+        if output_dir is None:
+            output_dir = os.getcwd()
+
+        coco1 = COCO(main_coco)
+        coco2 = COCO(sampled_coco)
+
+        # Find the maximum IDs used in the first JSON file
+        max_image_id_1 = max(image["id"] for image in coco1.dataset["images"])
+        max_annotation_id_1 = max(
+            annotation["id"] for annotation in coco1.dataset["annotations"]
+        )
+
+        # Increment IDs in the second JSON file to avoid conflicts
+        for image in coco2.dataset["images"]:
+            image["id"] += max_image_id_1 + 1
+
+        for annotation in coco2.dataset["annotations"]:
+            annotation["id"] += max_annotation_id_1 + 1
+            annotation["image_id"] += max_image_id_1 + 1
+
+        merged_categories = coco1.dataset["categories"] + coco2.dataset["categories"]
+        seen = set()
+        unique_merged_categories = []
+
+        for item in merged_categories:
+            # Create a tuple of id and name to check for uniqueness
+            key = (item["id"], item["name"])
+            if key not in seen:
+                unique_merged_categories.append(item)
+                seen.add(key)
+
+        # Merge annotations, images, categories, info, and licenses
+        merged_annotations = coco1.dataset["annotations"] + coco2.dataset["annotations"]
+        merged_images = coco1.dataset["images"] + coco2.dataset["images"]
+        merged_info = coco1.dataset["info"]
+        merged_licenses = coco1.dataset["licenses"]
+
+        merged_coco_data = {
+            "info": merged_info,
+            "licenses": merged_licenses,
+            "images": merged_images,
+            "annotations": merged_annotations,
+            "categories": unique_merged_categories,
+        }
+
+        # Save merged COCO JSON to output directory
+        output_file = os.path.join(output_dir, "merged_coco.json")
+        with open(output_file, "w") as f:
+            json.dump(merged_coco_data, f, indent=4)
+
+        return output_file
