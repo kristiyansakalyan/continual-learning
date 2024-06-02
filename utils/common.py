bb3f29c5d036d00ae5fb94477a0a94136e6d0ce5
Kristiyan Sakalyan <kristian.sakalian@gmail.com>
2024-06-02 22:44:12 +0200

    W&B Visualizations, postprocessing with BG

--- utils/common.py
+++ utils/common.py
@@ -142,7 +142,7 @@ preprocess_mask2former = partial(
 
 
 def m2f_extract_pred_maps_and_masks(
-    batch: Mask2FormerBatch, outputs: Any, processor: AutoImageProcessor
+    batch: Mask2FormerBatch, outputs: Any
 ) -> tuple[list[torch.Tensor], list[torch.Tensor]]:
     """Extract the predicted maps and masks for Mask2Former.
 
@@ -170,9 +170,10 @@ def m2f_extract_pred_maps_and_masks(
     ]
 
     # We can use that: https://github.com/huggingface/transformers/blob/main/src/transformers/models/mask2former/image_processing_mask2former.py
-    pred_maps = processor.post_process_semantic_segmentation(
-        outputs, target_sizes=target_sizes
-    )
+    pred_maps = post_process_semantic_segmentation(outputs, target_sizes=target_sizes)
+
+    for i in range(len(pred_maps)):
+        pred_maps[i][pred_maps[i] == 11] = 255
 
     # Get rid of warning in evaluate
     for i in range(len(pred_maps)):
@@ -210,3 +211,70 @@ def m2f_dataset_collate(examples):
         "mask_labels": mask_labels,
         "class_labels": class_labels,
     }
+
+
+def post_process_semantic_segmentation(
+    outputs, target_sizes: list[tuple[int, int]] | None = None
+) -> "torch.Tensor":
+    """
+    Converts the output of [`Mask2FormerForUniversalSegmentation`] into semantic segmentation maps. Only supports
+    PyTorch.
+
+    Args:
+        outputs ([`Mask2FormerForUniversalSegmentation`]):
+            Raw outputs of the model.
+        target_sizes (`List[Tuple[int, int]]`, *optional*):
+            List of length (batch_size), where each list item (`Tuple[int, int]]`) corresponds to the requested
+            final size (height, width) of each prediction. If left to None, predictions will not be resized.
+    Returns:
+        `List[torch.Tensor]`:
+            A list of length `batch_size`, where each item is a semantic segmentation map of shape (height, width)
+            corresponding to the target_sizes entry (if `target_sizes` is specified). Each entry of each
+            `torch.Tensor` correspond to a semantic class id.
+    """
+    class_queries_logits = (
+        outputs.class_queries_logits
+    )  # [batch_size, num_queries, num_classes+1]
+    masks_queries_logits = (
+        outputs.masks_queries_logits
+    )  # [batch_size, num_queries, height, width]
+
+    # # Scale back to preprocessed image size - (384, 384) for all models
+    masks_queries_logits = torch.nn.functional.interpolate(
+        masks_queries_logits, size=(270, 480), mode="bilinear", align_corners=False
+    )
+
+    # Remove the null class `[..., :-1]`
+    masks_classes = class_queries_logits.softmax(dim=-1)  # [..., :-1]
+    masks_probs = (
+        masks_queries_logits.sigmoid()
+    )  # [batch_size, num_queries, height, width]
+
+    # Semantic segmentation logits of shape (batch_size, num_classes, height, width)
+    segmentation = torch.einsum("bqc, bqhw -> bchw", masks_classes, masks_probs)
+    batch_size = class_queries_logits.shape[0]
+
+    # Resize logits and compute semantic segmentation maps
+    if target_sizes is not None:
+        if batch_size != len(target_sizes):
+            raise ValueError(
+                "Make sure that you pass in as many target sizes as the batch dimension of the logits"
+            )
+
+        semantic_segmentation = []
+        for idx in range(batch_size):
+            resized_logits = torch.nn.functional.interpolate(
+                segmentation[idx].unsqueeze(dim=0),
+                size=target_sizes[idx],
+                mode="bilinear",
+                align_corners=False,
+            )
+            semantic_map = resized_logits[0].argmax(dim=0)
+            semantic_segmentation.append(semantic_map)
+    else:
+        semantic_segmentation = segmentation.argmax(dim=1)
+        semantic_segmentation = [
+            semantic_segmentation[i] for i in range(semantic_segmentation.shape[0])
+        ]
+
+    return semantic_segmentation
---
+++ utils/wandb_utils.py
@@ -0,0 +1,52 @@
+import torch
+from torchvision.transforms.functional import to_pil_image
+
+import wandb
+from utils.dataset_utils import ZEISS_CATEGORIES
+
+ZEISS_WITH_BG = {k - 1: v for k, v in ZEISS_CATEGORIES.items()}
+BG = 255
+ZEISS_WITH_BG[BG] = "Background"
+
+
+def log_table_of_images(
+    table: wandb.Table,  # Should have ID and Image columns
+    pixel_values: torch.Tensor,
+    pixel_mean: torch.Tensor,
+    pixel_std: torch.Tensor,
+    pred_maps: list[torch.Tensor],
+    masks: list[torch.Tensor],
+    batch_index: int,
+) -> None:
+    batch_size = pixel_values.shape[0]
+    for INDEX in range(batch_size):
+        img = (
+            (
+                (pixel_values[INDEX, ...] * pixel_std.reshape(3, 1, 1))
+                + pixel_mean.reshape(3, 1, 1)
+            )
+            .permute(1, 2, 0)
+            .cpu()
+            .numpy()
+        )
+
+        pred_instance = pred_maps[INDEX].cpu().numpy()
+        gt_instance = masks[INDEX].cpu().numpy()
+
+        mask_img = wandb.Image(
+            to_pil_image(img),
+            masks={
+                "predictions": {
+                    "mask_data": pred_instance,
+                    "class_labels": ZEISS_WITH_BG,
+                },
+                "ground_truth": {
+                    "mask_data": gt_instance,
+                    "class_labels": ZEISS_WITH_BG,
+                },
+            },
+        )
+
+        img_index = (batch_index * batch_size) + INDEX
+
+        table.add_data(img_index, mask_img)
